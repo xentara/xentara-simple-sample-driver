1ca6d7ddaf392289ee9d431834e17e04070b7fdb
Georg Schwab <georg.schwab@xentara.io>
2023-06-09 14:50:02 +0200

    Fixed some comments

--- src/Events.hpp
+++ src/Events.hpp
@@ -3,13 +3,12 @@
 
 #include <xentara/process/Event.hpp>
 
-/// @brief Contains the Xentara events of the driver
 namespace xentara::samples::simpleDriver::events
 {
 
-/// @brief A Xentara event that is fired when an output was written
+// A Xentara event that is fired when an output was written
 extern const process::Event::Role kWritten;
-/// @brief A Xentara event that is fired when an error occurred writing an output
+// A Xentara event that is fired when an error occurred writing an output
 extern const process::Event::Role kWriteError;
 
 } // namespace xentara::samples::simpleDriver::events
--- src/Tasks.hpp
+++ src/Tasks.hpp
@@ -3,13 +3,12 @@
 
 #include <xentara/process/Task.hpp>
 
-/// @brief Contains the Xentara tasks of the driver
 namespace xentara::samples::simpleDriver::tasks
 {
 
-/// @brief A Xentara tasks used to read an input
+// A Xentara task used to read an input
 extern const process::Task::Role kRead;
-/// @brief A Xentara tasks used to write an output
+// A Xentara task used to write an output
 extern const process::Task::Role kWrite;
 
 } // namespace xentara::samples::simpleDriver::tasks
